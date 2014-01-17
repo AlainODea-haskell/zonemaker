061263da5be29397e60c1db36c309ea46975131e
Alain O'Dea <alain.odea@verafin.com>
2014-01-17 05:12:16 +0000

    WIP: Haskell DSL for Creating SmartMachines

---
+++ MachineJSON.hs
@@ -0,0 +1,115 @@
+module MachineJSON
+   ( machineJSON
+   , ip
+   , MachineSpec( MachineSpec )
+   , MachineProp( Brand
+                , ImageUUID
+                , MaxPhysicalMemory
+                , Hostname
+                , DNSDomain
+                , Resolvers
+                , Alias
+                , NICs
+                , CustomerMetadata )
+   , BrandValue ( Joyent
+                , KVM )
+   , IPAddress
+   , NIC( NIC )
+   , NICProp( NICTag
+            , Gateway
+            , IP
+            , Netmask )
+   , CustomerMetadataProp( RootAuthorizedKeys
+                         , UserScript )
+   , RootAuthorizedKey( RootAuthorizedKey )
+   ) where
+
+import           Data.Bits      (Bits, shiftL, shiftR, (.&.))
+import           Data.String    (IsString (fromString))
+import           Data.Text      (Text, intercalate, pack)
+import qualified Data.Text.IO   as T
+import           Data.Aeson.Types (ToJSON(toJSON), (.=), Value, object)
+import           Network.Socket (HostAddress)
+import Data.Char (toLower)
+
+machineJSON :: MachineSpec -> Value
+machineJSON (MachineSpec properties) =
+    object $ map toPair properties
+    where toPair (Brand x)             = "brand" .= x
+          toPair (ImageUUID x)         = "image_uuid" .= x
+          toPair (MaxPhysicalMemory x) = "max_physical_memory" .= x
+          toPair (Hostname x)          = "hostname" .= x
+          toPair (DNSDomain x)         = "dns_domain" .= x
+          toPair (Resolvers x)         = "resolvers" .= x
+          toPair (Alias x)             = "alias" .= x
+          toPair (NICs x)              = "nics" .= x
+          toPair (CustomerMetadata x)  =
+              "customer_metadata" .= (object $ map metaPair x)
+          metaPair (RootAuthorizedKeys xs) =
+              "root_authorized_keys" .= intercalate "\n" (map unbox xs)
+          metaPair (UserScript x) =
+              "user_script" .= x
+          unbox (RootAuthorizedKey x) = x
+
+newtype MachineSpec = MachineSpec [MachineProp]
+data MachineProp =
+      Brand BrandValue
+    | ImageUUID Text
+    | MaxPhysicalMemory Int
+    | Hostname Text
+    | DNSDomain Text
+    | Resolvers [IPAddress]
+    | Alias Text
+    | NICs [NIC]
+    | CustomerMetadata [CustomerMetadataProp]
+
+data BrandValue =
+      Joyent
+    | KVM 
+    deriving Show
+instance ToJSON BrandValue where
+    toJSON = toJSON . map toLower . show
+
+newtype IPAddress = IPAddress HostAddress
+    deriving (Num, Eq, Data.Bits.Bits)
+
+newtype NIC = NIC [NICProp]
+data NICProp =
+      NICTag Text
+    | Gateway IPAddress
+    | IP IPAddress
+    | Netmask IPAddress
+
+data CustomerMetadataProp =
+      RootAuthorizedKeys [RootAuthorizedKey]
+    | UserScript Text
+newtype RootAuthorizedKey = RootAuthorizedKey Text
+
+ip :: (Num a, Bits a) => a -> a -> a -> a -> a
+ip oct1 oct2 oct3 oct4 = oct1 `shiftL` 24 + oct2 `shiftL` 16 + oct3 `shiftL` 8 + oct4
+
+instance Show IPAddress where
+    show (IPAddress x) = show x
+
+instance ToJSON IPAddress where
+    toJSON x = toJSON $ intercalate "." [showByte 0 x, showByte 1 x, showByte 2 x, showByte 3 x]
+      where showByte n x = pack . show $ byte n x
+            byte :: Int -> IPAddress -> IPAddress
+            byte n x | n == 0 = x `shiftR` 24
+                     | n == 1 = (x .&. 255 `shiftL` 16) `shiftR` 16
+                     | n == 2 = (x .&. 255 `shiftL` 8) `shiftR` 8
+                     | n == 3 = x .&. 255
+                     | otherwise = 0
+
+instance ToJSON NIC where
+    toJSON (NIC props) = object $ map toPair props
+      where toPair (NICTag nicTag)   = "nic_tag" .= nicTag
+            toPair (Gateway gateway) = "gateway" .= gateway
+            toPair (IP ip)           = "ip" .= ip
+            toPair (Netmask netmask) = "netmask" .= netmask
+
+instance ToJSON CustomerMetadataProp where
+    toJSON (RootAuthorizedKeys xs) = object [
+        "root_authorized_keys" .= intercalate "\n" (map unbox xs)
+      ]
+      where unbox (RootAuthorizedKey x) = x
---
+++ Main.hs
@@ -0,0 +1,58 @@
+import MachineJSON
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString.Lazy as BL
+import Data.Aeson (encode)
+import Shelly
+import Prelude hiding (FilePath)
+default (T.Text)
+
+main = do
+  createTestMachine "tomcat01.alainodea.local" $ ip 192 168 2 22
+
+createTestMachine :: String -> IPAddress -> IO ()
+createTestMachine = createMachine classBProdnet users
+
+classBProdnet :: IPAddress -> NIC
+classBProdnet addr =
+  NIC [
+    NICTag "external"
+  , Gateway $ ip 192 168  2  1
+  , IP addr
+  , Netmask $ ip 255 255 255 0
+  ]
+
+users :: [RootAuthorizedKey]
+users = [
+    alain_odea
+  ]
+
+alain_odea :: RootAuthorizedKey
+alain_odea = RootAuthorizedKey "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCTLDnggGlVWHOrlbEpLP8ZCmUQrUwq13+x+e216gghxQi+IZiEfTpQWoFqz5opDVgNzRdBWfMcS6qhBK1YyIt/I+W6tZTsvR3Ncq6XS0EATdSFZWozpgrC5jFnR1fAAoAJCDqcUs7lMd2Fg5MJe3y9PTlcFP+WOxfN/3zwqd1kH0hCJGLZbS3LgB7IaxLQylLwa6MzOvsw/65s24whGfGCW6Fgh/tsC2W2wLzKix6qNg8ypoBYtBZqtx41quDDfg62bDX53CXgqDxiYtUvQGUA7jvALtig8ttAZyZdWbAIgKKwURZjQIFIMMPhpT6eHkSIQ3qXNkh2qvlOmvM4s2LJ alain.odea@gmail.com"
+
+createMachine :: (IPAddress -> NIC) -> [RootAuthorizedKey] -> String -> IPAddress -> IO ()
+createMachine nicBuilder keys hostname hostIp = shelly $ do
+  setStdin $ T.decodeUtf8 . BL.toStrict . encode . machineJSON $ MachineSpec [
+      Brand Joyent
+    , ImageUUID imageUuid
+    , MaxPhysicalMemory maxMemory
+    , Hostname $ T.pack hostname
+    , DNSDomain $ T.pack dnsDomain
+    , Resolvers resolvers
+    , Alias alias
+    , NICs [nicBuilder hostIp]
+    , CustomerMetadata [
+        RootAuthorizedKeys keys
+      , UserScript "/usr/sbin/mdata-get root_authorized_keys > ~root/.ssh/authorized_keys ; /usr/sbin/mdata-get root_authorized_keys > ~admin/.ssh/authorized_keys"
+      ]
+    ]
+  run_ "cat" []
+--run_ "ssh" ["root@smartos01", "vmadm", "create"]
+
+--resolvers = [ip 192 168 2 2, ip 192 168 2 3]
+resolvers = [ ip 208 67 222 222
+            , ip 208 67 220 220 ]
+imageUuid = "c353c568-69ad-11e3-a248-db288786ea63"
+maxMemory = 2048
+dnsDomain = "alainodea.local"
+alias = "tomcat"
